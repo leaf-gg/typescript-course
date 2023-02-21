23d56cc15b40a4d6fe76819a2b7d3a1a6a4b53dc
Luiz Guilherme <jobs.lugui@gmail.com>
2023-02-21 17:57:27 -0300

    More events

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 "use strict";
+/*
 // button
+
 const button = document.querySelector('button');
-function handleClick(event) {
-    console.log(event, 'event');
+
+function handleClick(event: MouseEvent) {
+  console.log(event, 'event');
 }
+
 button?.addEventListener('click', handleClick);
+
 // scroll
-function handleScroll(event) {
-    console.log(event);
+
+function handleScroll(event: Event) {
+  console.log(event);
 }
 window.addEventListener('scroll', handleScroll);
-function enableMenu(event) {
-    if (event instanceof MouseEvent) {
-        console.log(event.pageX);
-    }
-    if (event instanceof TouchEvent) {
-        console.log(event.touches[0].pageX);
-    }
+
+function enableMenu(event: Event) {
+  if (event instanceof MouseEvent) {
+    console.log(event.pageX);
+  }
+  if (event instanceof TouchEvent) {
+    console.log(event.touches[0].pageX);
+  }
 }
 document.documentElement.addEventListener('mousedown', enableMenu);
 document.documentElement.addEventListener('touchstart', enableMenu);
 window.addEventListener('keydown', enableMenu);
+
+
+
+const button = document.querySelector('button');
+function handleClick(this: HTMLButtonElement, event: MouseEvent) {
+  console.log(this);
+}
+
+
+*/
+const button = document.querySelector('button');
+function handleClick(event) {
+    const el = event.currentTarget;
+    if (el instanceof HTMLElement) {
+        console.log(el.innerText);
+    }
+}
+button?.addEventListener('click', handleClick);
--- script.ts
+++ script.ts
@@ -1,3 +1,4 @@
+/*
 // button
 
 const button = document.querySelector('button');
@@ -26,3 +27,23 @@ function enableMenu(event: Event) {
 document.documentElement.addEventListener('mousedown', enableMenu);
 document.documentElement.addEventListener('touchstart', enableMenu);
 window.addEventListener('keydown', enableMenu);
+
+
+
+const button = document.querySelector('button');
+function handleClick(this: HTMLButtonElement, event: MouseEvent) {
+  console.log(this);
+}
+
+
+*/
+
+const button = document.querySelector('button');
+function handleClick(event: MouseEvent) {
+  const el = event.currentTarget;
+  if (el instanceof HTMLElement) {
+    console.log(el.innerText);
+  }
+}
+
+button?.addEventListener('click', handleClick);
