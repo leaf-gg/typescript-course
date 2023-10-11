489f558d9905ffc048090d9974ecb31feb742da8
Luiz Guilherme <jobs.lugui@gmail.com>
2023-10-11 00:01:11 -0300

    feat: slider thumbs

--- src/Slide.ts
+++ src/Slide.ts
@@ -8,6 +8,10 @@ export default class Slide {
     index: number;
     slide: Element;
     timeout: Timeout | null;
+    pausedTimeout: Timeout | null;
+    paused: boolean;
+    thumbItems: HTMLElement[] | null;
+    thumb: HTMLElement | null;
     
   constructor(
     container: Element,
@@ -19,40 +23,96 @@ export default class Slide {
     this.slides = slides;
     this.controls = controls;
     this.time = time;
-    this.index = 0;
-    this.slide = this.slides[this.index];
-    this.showSlide(this.index)
     this.timeout = null;
+    this.pausedTimeout = null;
+    this.index = localStorage.getItem("activeSlide") ? Number(localStorage.getItem("activeSlide")): 0;
+    this.slide = this.slides[this.index];
+    this.paused = false;
+    this.thumbItems = null;
+    this.thumb = null;
+
     this.init()
   }
 
   hide(el: Element){
     el.classList.remove('active')
+    
+    if(el instanceof HTMLVideoElement){
+      el.currentTime = 0;
+      el.pause();
+    }
   }
 
   showSlide(index: number){
     this.index = index;
     this.slide = this.slides[this.index]
+    localStorage.setItem("activeSlide", String(this.index));
+
+    if(this.thumbItems){
+      this.thumb = this.thumbItems[this.index]
+      this.thumbItems.forEach(el => el.classList.remove('active'))
+      this.thumb.classList.add('active');
+    }
+
     this.slides.forEach(el => this.hide(el))
     this.slide.classList.add('active')
-    this.auto(this.time)
+
+    if ( this.slide instanceof HTMLVideoElement){
+      this.autoVideo(this.slide)   
+    } else {
+      this.auto(this.time)
+    }
   }
 
+  autoVideo(video: HTMLVideoElement){
+    video.muted = true;
+    video.play();
+    let firstPlay = true;
+    video.addEventListener("playing", () => {
+      if(firstPlay) this.auto(video.duration * 1000)
+        firstPlay = false;
+      
+    })
+  }
+ 
   auto(time: number){
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), time);
-    // const id = setTimeout(() => this.next(), time);
-    // clearTimeout(id)
+    if(this.thumb) this.thumb.style.animationDuration = `${time}ms`;
   }
 
   prev(){
+    if(this.paused) return;
     const prev = (this.index > 0 ? this.index - 1 : this.slides.length - 1);
     this.showSlide(prev)
   }
 
   next(){
+    if(this.paused) return;
     const next = (this.index + 1) < this.slides.length ? this.index + 1 : 0;
     this.showSlide(next)
+  }
+
+  pause(){
+    console.log('paused');
+    this.pausedTimeout = new Timeout(() =>{
+      this.timeout?.pause();
+      this.thumb?.classList.add('paused');
+      this.paused = true;
+      if(this.slide instanceof HTMLVideoElement) this.slide.pause();
+    }, 300)
+  }
+
+  continue(){
+    console.log('continue');
+    this.pausedTimeout?.clear();
+    this.thumb?.classList.remove('paused');
+
+    if(this.paused){
+      this.paused = false;
+      this.timeout?.continue();
+      if(this.slide instanceof HTMLVideoElement) this.slide.play();
+    }
   }
 
   private addControls(){
@@ -63,12 +123,25 @@ export default class Slide {
     prevBtn.innerText = "Previous Slide";
     nextBtn.innerHTML = "Next Slide"
     
+    this.controls.addEventListener("pointerdown", () => this.pause());
+    this.controls.addEventListener("pointerup", () => this.continue());
+
+
     prevBtn.addEventListener("pointerup", () => this.prev())
     nextBtn.addEventListener("pointerup", () => this.next())
   }
-  
+  private addThumbItems(){
+    const thumbContainer = document.createElement('div');
+    thumbContainer.id = "slide-thumb";
+    for( let i = 0; i < this.slides.length; i++){
+      thumbContainer.innerHTML += '<span><span class="thumb-item"></span></span>';
+    }
+    this.controls.appendChild(thumbContainer);
+    this.thumbItems = Array.from(document.querySelectorAll('.thumb-item'))
+  }
   private init(){
     this.addControls();
+    this.addThumbItems();
     this.showSlide(this.index);
   }
 
